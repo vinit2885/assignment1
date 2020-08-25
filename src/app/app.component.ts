000839314f915c86468b6480270a1c3b3542a1ca
Vinit <vinit.agrawal@netcore.co.in>
2020-08-25 22:26:45 +0530

    assignment3 changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assignment1';
-  username = 'Vinit';
+ displayflag =false;
+ randomtxt = false;
+ servers = [];
+ counter=0;
+
+ togglevalue(){
+   this.servers.push('Server - '+Math.random());
+   console.log(this.servers);
+   this.randomtxt = !this.randomtxt;
+
+ }
+ addservers(){
+   this.counter++;
+  this.servers.push('Server - '+this.counter);
+  console.log(this.servers);
+ }
 
 }
